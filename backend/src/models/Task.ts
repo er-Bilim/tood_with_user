21cf99792656daa6ce137ede69219ca38a215210
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-24 04:03:57 +0600

    feat(backend): add uniq title validation and user existence check

--- backend/src/models/Task.ts
+++ backend/src/models/Task.ts
@@ -1,16 +1,45 @@
 import { model, Schema } from 'mongoose';
+import User from './user/User.ts';
 
 const TaskSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     required: true,
+
+    validate: {
+      validator: async (user_id: string) => {
+        const user = await User.findById(user_id);
+
+        if (!user) {
+          return false;
+        }
+
+        return true;
+      },
+      message: 'User with this id not exists :(',
+    },
   },
   title: {
     type: String,
     required: true,
     min: 1,
     max: 25,
+    unique: true,
+
+    validate: {
+      validator: async (title: string) => {
+        const task = await Task.findOne({ title });
+
+        if (task) {
+          return false;
+        }
+
+        return true;
+      },
+
+      message: 'Task with this title already exists :(',
+    },
   },
   description: {
     type: String,
